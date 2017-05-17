151deb847b2ba5e66d0e70ef054cd938eaeb7c96
David Scott <dave@recoil.org>
2017-05-17 11:48:57 +0100

    test: add cases for {,valid} {Metric,Label}Names
    Signed-off-by: David Scott <dave@recoil.org>

--- tests/test.ml
+++ tests/test.ml
@@ -27,6 +27,77 @@ let test_metrics () =
     "
     output
 
+
+(* "^[a-zA-Z_][a-zA-Z0-9_]*$" *)
+let valid_labels = [
+  "_";
+  "a";
+  "aA0b1B9c8C7z6Z5y4Y3x2X1";
+  "_______";
+]
+
+let invalid_labels = [
+  "";
+  "1";
+  "a bad label";
+]
+
+let check_valid_label label () =
+  let _l: LabelName.t = LabelName.v label in
+  ()
+
+let check_invalid_label label () =
+  let valid =
+    try
+      let _l: LabelName.t = LabelName.v label in
+      true
+    with _ -> false in
+  if valid then
+    failwith (label ^ " should be an invalid label")
+
+let test_valid_labels_set = List.map (fun label ->
+  label, `Quick, check_valid_label label
+) valid_labels
+
+let test_invalid_labels_set = List.map (fun label ->
+  label, `Quick, check_invalid_label label
+) invalid_labels
+
+let check_valid_metric metric () =
+  let _m: MetricName.t = MetricName.v metric in
+  ()
+
+let check_invalid_metric metric () =
+  let valid =
+    try
+      let _m: MetricName.t = MetricName.v metric in
+      true
+    with _ -> false in
+  if valid then
+    failwith (metric ^ " should be an invalid metric")
+
+(* "^[a-zA-Z_:][a-zA-Z0-9_:]*$"  *)
+let valid_metrics = [
+  "_";
+  ":";
+  "aA0b1B9c8C7z6Z5y4Y3x:2X1";
+  ":::::::";
+]
+
+let invalid_metrics = [
+  "";
+  "1";
+  "a bad metric";
+]
+
+let test_valid_metrics_set = List.map (fun metric ->
+  metric, `Quick, check_valid_metric metric
+) valid_metrics
+
+let test_invalid_metrics_set = List.map (fun metric ->
+  metric, `Quick, check_invalid_metric metric
+) invalid_metrics
+
 let test_set = [
   "Metrics", `Quick, test_metrics;
 ]
@@ -34,4 +105,8 @@ let test_set = [
 let () =
   Alcotest.run "prometheus" [
     "main", test_set;
+    "valid_labels", test_valid_labels_set;
+    "invalid_labels", test_invalid_labels_set;
+    "valid_metrics", test_valid_metrics_set;
+    "invalid_metrics", test_invalid_metrics_set;
   ]
