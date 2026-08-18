322c4a5b112c8c0ca9af69252d15b6c97159fbd0
Thomas Leonard <talex5@gmail.com>
2026-08-18 10:53:52 +0100

    prometheus-eio: allow specifying which registry to serve
    Also, add a test for the server.

--- eio/prometheus_eio.ml
+++ eio/prometheus_eio.ml
@@ -1,9 +1,9 @@
 (* Eio support for Prometheus. *)
 
-let callback _conn req _body =
+let callback ?(registry=Prometheus.CollectorRegistry.default) () _conn req _body =
   match Http.Request.meth req, Http.Request.resource req with
   | `GET, "/metrics" ->
-    let data = Prometheus.CollectorRegistry.(collect default) in
+    let data = Prometheus.CollectorRegistry.collect registry in
     let body = Fmt.to_to_string Prometheus_reporter.TextFormat_0_0_4.output data in
     let headers =
       Http.Header.of_list [ "content-type", "text/plain; version=0.0.4" ]
--- eio/prometheus_eio.mli
+++ eio/prometheus_eio.mli
@@ -1,11 +1,13 @@
 (** Eio support for Prometheus. *)
 
 val callback :
+  ?registry:Prometheus.CollectorRegistry.t ->
+  unit ->
   Cohttp_eio.Server.conn ->
   Http.Request.t ->
   Cohttp_eio.Server.body ->
   Cohttp_eio.Server.response
-(** [callback] is a cohttp-eio request handler that serves
+(** [callback ()] is a cohttp-eio request handler that serves
     {!Prometheus.CollectorRegistry.default} at [/metrics] in the text format
     and answers anything else with [400 Bad Request]. Build and run a server
     with
